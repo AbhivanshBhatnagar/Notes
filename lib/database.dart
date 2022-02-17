1d43c7acb3eb50838fb9b1fdc058d278f19a4b40
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2022-02-17 16:24:53 +0530

    added firestore and firebase and CRUD functions

---
+++ lib/database.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _collectionReference = _firestore.collection("notes");
+
+class Database {
+  static String? UserID;
+  static Future<void> addItem(
+      {required String Title, required String Description}) async {
+    DocumentReference documentReferencer =
+        _collectionReference.doc(UserID).collection("notes").doc();
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": Title,
+      "description": Description,
+    };
+    await documentReferencer
+        .set(data)
+        .whenComplete(() => Dialog(
+              child: Text("Succesfully Added Note"),
+            ))
+        .catchError((e) => Dialog(
+              child: Text("Some Error Occured"),
+            ));
+  }
+
+  static Stream<QuerySnapshot> readItems() {
+    CollectionReference notesItemCollection =
+        _collectionReference.doc(UserID).collection("notes");
+    return notesItemCollection.snapshots();
+  }
+
+  static Future<void> updateItem({
+    required String title,
+    required String description,
+    required String docId,
+  }) async {
+    DocumentReference documentReferencer =
+        _collectionReference.doc(UserID).collection('items').doc(docId);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": title,
+      "description": description,
+    };
+
+    await documentReferencer
+        .update(data)
+        .whenComplete(() => print("Note item updated in the database"))
+        .catchError((e) => print(e));
+  }
+
+  static Future<void> deleteItem({required String docId}) async {
+    DocumentReference documentReferencer =
+        _collectionReference.doc(UserID).collection("notes").doc(docId);
+    await documentReferencer
+        .delete()
+        .whenComplete(() => Dialog(
+              child: Text("Note Deleted Succesfully"),
+            ))
+        .catchError((e) => Dialog(
+              child: Text(e),
+            ));
+  }
+}
